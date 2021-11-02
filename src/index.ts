2e471fb060a7c0bdf353e851b4fdfd369100abf2
richa-postnl <79089245+richa-postnl@users.noreply.github.com>
2021-11-02 01:02:32 +0100

    chore: wip added test and code cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,44 @@
-const world = 'world';
+let lineNumber = 1;
+let testCase = 0;
+// let rows = 0;
+// let cols = 0;
 
-export function hello(): string {
-  return `Hello ${world}! `;
+const listener = () => {
+  process.stdin.on("data", (val: Buffer) => {
+    switch(lineNumber) {
+      case 1:
+        processTestCase(val.toString());
+        break;
+      case 2:
+        setRowsAndCols(val.toString());
+        break;
+      case 3:
+        processData(val.toString());
+        break;
+      default: 
+        console.log("ENDING");
+    }
+    lineNumber++;
+  });
+}
+
+function processData(val: string) {
+  console.log("processData")
+}
+
+function processTestCase(val: string) {
+  console.log("processTestCase")
+  testCase = Number(val);
+}
+
+function setRowsAndCols(val: string) {
+  console.log("setRowsAndCols")
+  // let splitString = val.split(" ");
+  // rows = +splitString[0];
+  // cols = +splitString[1];
+  testCase++;
+}
+
+export function exec() {
+  listener();
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,8 +1,18 @@
-import {hello} from '../src/index';
+import {exec} from '../src/index';
+const mockExit = jest.spyOn(process, 'exit')
+.mockImplementation((number) => { throw new Error('process.exit: ' + number); });
+
+import * as stdin from 'mock-stdin';
+const mockStdin = stdin.stdin();
 
 describe('index', () => {
-
     it('should return correct values', () => {
-        hello();
+        exec();
+        mockStdin.send("1", "ascii");
+        mockStdin.send("3 4", "ascii");
+        mockStdin.send("0001", "ascii");
+        mockStdin.send("0011", "ascii");
+        mockStdin.send("0110", "ascii");
+        mockStdin.end();
     });
 });
